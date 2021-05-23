bcd17b93b7dfc5ae7ac67f657fcc02b0fbbd40f4
Harshit-Raj-2000 <52253887+Harshit-Raj-2000@users.noreply.github.com>
2021-05-24 02:43:15 +0530

    added favicon

--- poll/urls.py
+++ poll/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     # path("login", views.login_view, name="login_view"),
     # path("signup", views.signup_view, name="signup_view"),
+    
     path("create", views.create, name="create"),
     path("view/<int:poll_id>", views.view_poll, name="view"),
     path("results/<int:poll_id>", views.results_view, name="results")
